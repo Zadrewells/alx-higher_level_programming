f4eeeb66e3cec0535bfc6c01223a3d2ff2695dbf
Zadrewlls <zadrewells@gmail.com>
2022-08-31 14:55:55 +0000

    fix

--- 0x04-python-more_data_structures/7-update_dictionary.py
+++ 0x04-python-more_data_structures/7-update_dictionary.py
@@ -1,10 +1,4 @@
-
 #!/usr/bin/python3
-# 7-update_dictionary.py
-# Zadre Wells <zadrewells@gmail.com>
-
-
 def update_dictionary(a_dictionary, key, value):
-    """Replace or add key/value pairs in a dictionary."""
     a_dictionary[key] = value
     return (a_dictionary)
