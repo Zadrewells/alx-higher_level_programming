9edbf48caba72769fba4752951b26b97d677e88e
Zadrewells <zadrewells@gmail.com>
2022-12-15 14:40:31 -0800

    fix

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -1,13 +1,8 @@
 #!/usr/bin/python3
-"""
-    Fetches https://intranet.hbtn.io/status
-"""
+"""fetches https://alx-intranet.hbtn.io/status"""
 import requests
 
-
-if __name__ == "__main__":
-    resp = requests.get('https://intranet.hbtn.io/status')
-    if resp is not None:
-        print("Body response:")
-        print("\t- type: {}".format(type(resp.text)))
-        print("\t- content: {}".format(resp.text))
+data = requests.get("https://alx-intranet.hbtn.io/status")
+print("Body response:")
+print("\t- type: {}".format(type(data.text)))
+print("\t- content: {}".format(data.text))
