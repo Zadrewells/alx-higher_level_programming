c31f9aa8dbdd4344156b1f6fe3932a619a4d90bc
Zadrewells <zadrewells@gmail.com>
2023-01-17 10:40:34 +0000

    a script that gets the contents of a webpage and stores it in a file

---
+++ 0x14-javascript-web_scraping/5-request_store.js
@@ -0,0 +1,15 @@
+
+f a webpage and stores it in a file
+*/
+
+const axios = require('axios').default;
+
+axios.get(process.argv[2])
+  .then((response) => {
+    const f = process.argv[3];
+    const data = response.data;
+    const fs = require('fs');
+    fs.writeFile(f, data, (err) => {
+      err && console.log(err);
+    });
+  });
