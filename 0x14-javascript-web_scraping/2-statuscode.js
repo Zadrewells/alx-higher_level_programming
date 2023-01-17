927b81cc9091ba1007e9d130ce439b2a8eaa275a
Zadrewells <zadrewells@gmail.com>
2023-01-17 10:57:11 +0000

    a script that prints all characters of a Star Wars movie

--- 0x14-javascript-web_scraping/2-statuscode.js
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -1,29 +1,5 @@
-
 #!/usr/bin/node
-/*
-display the status code of a GET request
-*/
-
-const axios = require('axios').default;
-
-axios.get(process.argv[2])
-  .then(function (response) {
-    console.log('code:', response.status);
-  })
-  .catch(function (err) {
-    console.log('code:', err.response.status);
-  });
-#!/usr/bin/node
-/*
-display the status code of a GET request
-*/
-
-const axios = require('axios').default;
-
-axios.get(process.argv[2])
-  .then(function (response) {
-    console.log('code:', response.status);
-  })
-  .catch(function (err) {
-    console.log('code:', err.response.status);
-  });
+const request = require('request');
+request.get(process.argv[2]).on('response', function (response) {
+  console.log(`code: ${response.statusCode}`);
+});
--- 0x14-javascript-web_scraping/3-starwars_title.js
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -1,16 +1,14 @@
-#!/usr/bin/node
-/*
-Prints the title of a Star Wars movie where
-the episode number matches a given integer
-*/
+t request = require('request');
+const episodeNum = process.argv[2];
+const API_URL = 'https://swapi-api.hbtn.io/api/films/';
 
-const axios = require('axios').default;
-
-const starWars = 'https://swapi-api.hbtn.io/api/films/'.concat(process.argv[2]);
-axios.get(starWars)
-  .then(function (response) {
-    console.log(response.data.title);
-  })
-  .catch(function (err) {
+request(API_URL + episodeNum, function (err, response, body) {
+  if (err) {
     console.log(err);
-  });
+  } else if (response.statusCode === 200) {
+    const responseJSON = JSON.parse(body);
+    console.log(responseJSON.title);
+  } else {
+    console.log('Error code: ' + response.statusCode);
+  }
+});
